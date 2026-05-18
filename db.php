ef7dd45864fe8ce53214c37ae752c3dab4c79ac2
AlejandroJUPS <agrajales059@gmail.com>
2026-05-17 21:07:42 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,16 @@
 <?php
-// db.php - Database connection
-$DB_HOST = "sql103.infinityfree.com";
-$DB_USER = "if0_40909083";
-$DB_PASS = "l26odsVdk4PSgga";
-$DB_NAME = "if0_40909083_emutable";
 
-$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
-if ($conn->connect_error) {
-    http_response_code(500);
-    exit("DB_ERROR");
+$host = "sql206.infinityfree.com";
+$user = "if0_41949366";
+$pass = "o1z4riVIZM6ui";
+$db   = "if0_41949366_emutable";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    die("Error de conexión: " . mysqli_connect_error());
 }
-$conn->set_charset("utf8mb4");
+
+mysqli_set_charset($conn, "utf8mb4");
+
 ?>
